241d172a2207bf5e117a6511376586d481db364d
destiny32601 <destiny32601@gmail.com>
2023-07-09 22:06:09 -0400

    getting my header and navigation to show up on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Destiny Dardon</h1>
+            <Navigation />
+        </header>
+    );
+};
+
+export default Header;
