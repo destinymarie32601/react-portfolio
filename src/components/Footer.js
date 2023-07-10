232a44990dc76cbf862fedaa9461e9adebe14d0e
destiny32601 <destiny32601@gmail.com>
2023-07-10 00:21:01 -0400

    implemented react icons in the footer.js file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {FaGithub, FaLinkedin, FaStackOverflow} from 'react-icons/fa';
 
 function Footer() {
     return (
         <footer>
-            <p>My personal footer</p>
+            <div className="footer-content">
+                <div className="social-icons">
+                    <a href = "https://github.com/destinymarie32601" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                    <a href = "https://github.com/destinymarie32601" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin />
+                    </a>
+                    <a href = "https://github.com/destinymarie32601" target="_blank" rel="noopener noreferrer">
+                        <FaStackOverflow />
+                    </a>
+                </div>
+            </div>
+            
         </footer>
     );
 }
