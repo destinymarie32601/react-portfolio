0027302989693d0e5d58dd672717b812e213239e
destiny32601 <destiny32601@gmail.com>
2023-07-10 01:19:12 -0400

    added a color scheme to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {color1, color2, color4} from './colors';
 
 function About() {
+    const aboutStyle = {
+        backgroundColor: color1,
+        color: color2
+    };
     return(
-        <section className ="about">
+        <section className ="about" style={aboutStyle}>
             <h2>About Me</h2>
             <div className="profile">
                 <img src="path/to/my/image.jpg" alt = "Profile"/>
